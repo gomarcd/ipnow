0d4356d9ec410c1689da2b6c97be68dd77123b3a
gomarcd <git@gomarcd.dev>
2025-03-27 17:13:10 -0600

    add whois to ui

--- src/index.ts
+++ src/index.ts
@@ -237,13 +237,6 @@ export default {
 								<span id="browser-info"></span>, <span id="os-info"></span><BR>
 							</div>
 
-					        <div class="whois-section" id="whois-section">
-					            <h3>WHOIS Lookup</h3>
-					            <input type="text" id="whois-input" placeholder="Search for IP or domain" />
-					            <button id="whois-button">Lookup</button>
-					            <pre id="whois-result" class="hidden"></pre>
-					        </div>
-
 							<div class="footer">
 								<div class="info" id="infobutton">
 									<span ><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
@@ -383,27 +376,6 @@ export default {
 						if (platform !== "Unknown platform" && browser !== "Unknown browser") {
 							deviceSection.classList.remove('hidden');
 						}
-
-			            // Event listener for WHOIS lookup
-			            const whoisButton = document.getElementById('whois-button');
-			            whoisButton.addEventListener('click', async () => {
-			                const whoisInput = document.getElementById('whois-input').value;
-			                const whoisResult = document.getElementById('whois-result');
-			                whoisResult.textContent = 'Fetching WHOIS data...';
-			                whoisResult.classList.remove('hidden');
-
-			                try {
-			                    const response = await fetch('/whois/' + whoisInput);
-			                    if (response.ok) {
-			                        const data = await response.json();
-			                        whoisResult.textContent = JSON.stringify(data, null, 2);
-			                    } else {
-			                        whoisResult.textContent = 'Error fetching WHOIS data';
-			                    }
-			                } catch (error) {
-			                    whoisResult.textContent = 'Error fetching WHOIS data';
-			                }
-			            });
 					});
 
 					function copyToClipboard(elementId, iconId) {
